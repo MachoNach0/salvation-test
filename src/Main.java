d5462c0d6acb069ca556f9eaab2cc1578fcc516b
Clyde Fernandes <clyde@Clydes-MacBook-Pro.local>
2023-02-09 15:57:54 +0400

    This is my first test file please forgive me lord giovanni my fan fiction is not panning out, the tentacles are real, coca cola is sin, cash me outside she said tenatively with love and affection

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("There are many people that have made magic");
+        System.out.println("therein lies the pain in my heart that makes me feel so regardless");
+        System.out.println("touche fagirantiesret");
+
+        ArrayList <String> holderOfString = new ArrayList<>();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+
+        for (int i = 0; i < 3; i++) {
+            
+
+        }
+    }
+
+}
